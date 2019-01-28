f7c133775f3a722013c225e0b12b35bd77869267
rever4433 <gao2524289@outlook.com>
2019-01-28 16:33:50 +0800

    新增user实体类

---
+++ src/main/java/com/rever/ehcachedemo/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.rever.ehcachedemo.mapper;
+
+import com.rever.ehcachedemo.entity.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author gaoyakang
+ * @since 2019-01-28
+ */
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
