73a9726dc575342e2f39f93972f781fcf76b5e5c
rever4433 <gao2524289@outlook.com>
2019-01-28 16:32:49 +0800

    新增userService

---
+++ src/main/java/com/rever/ehcachedemo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.rever.ehcachedemo.service;
+
+
+import com.baomidou.mybatisplus.service.IService;
+import com.rever.ehcachedemo.entity.User;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author gaoyakang
+ * @since 2019-01-28
+ */
+public interface UserService extends IService<User> {
+
+    /**
+     * 获取所有用户
+     *
+     * @return
+     */
+    List<User> findUserList();
+}
