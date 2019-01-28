6860933a8050d12379b172d00ae2739dbf4b7785
rever4433 <gao2524289@outlook.com>
2019-01-28 16:32:14 +0800

    新增user实体类

---
+++ src/main/java/com/rever/ehcachedemo/entity/User.java
@@ -0,0 +1,37 @@
+package com.rever.ehcachedemo.entity;
+
+import java.util.Date;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.enums.IdType;
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotations.Version;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author gaoyakang
+ * @since 2019-01-28
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    @TableId(value = "id", type = IdType.UUID)
+    private Integer id;
+    private String desc;
+    private Date time;
+    private String password;
+
+
+}
