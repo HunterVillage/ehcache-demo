bde1c8359342b09ca4800e7ad2a272d456777535
rever4433 <gao2524289@outlook.com>
2019-02-11 15:58:32 +0800

    新增创建userController

--- src/main/java/com/rever/ehcachedemo/controller/UserController.java
+++ src/main/java/com/rever/ehcachedemo/controller/UserController.java
@@ -4,10 +4,9 @@ package com.rever.ehcachedemo.controller;
 import com.rever.ehcachedemo.entity.User;
 import com.rever.ehcachedemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,13 +20,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @GetMapping
     public List<User> getUserList() {
         return userService.findUserList();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        user.setTime(new Date());
+        boolean result=this.userService.insert(user);
+        return result?user:null;
     }
 }
 
