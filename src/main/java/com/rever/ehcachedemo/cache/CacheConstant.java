86ff3c7a3e58fb9780cf27d364d4cc6f0705ed30
rever4433 <gao2524289@outlook.com>
2019-02-12 16:45:58 +0800

    新增缓存常量

---
+++ src/main/java/com/rever/ehcachedemo/cache/CacheConstant.java
@@ -0,0 +1,21 @@
+package com.rever.ehcachedemo.cache;
+
+/**
+ * @Description: 缓存常量
+ * @Author: gaoyakang
+ * @Version: 1.0
+ * @Create Date Time: 2019-02-12 16:01
+ * @Update Date Time:
+ *
+ * @see
+ */
+public class CacheConstant {
+
+    private CacheConstant(){}
+
+    /**
+     * 公用缓存
+     */
+    public static final String EHCACHE_COMMON_NAME = "commonCache";
+
+}
