e45afc4b2725605e51c5d12b3bf2f69e3bfd4975
rever4433 <gao2524289@outlook.com>
2019-02-12 16:45:31 +0800

    新增user缓存

--- src/main/java/com/rever/ehcachedemo/cache/AbstractCacheFactory.java
+++ src/main/java/com/rever/ehcachedemo/cache/AbstractCacheFactory.java
@@ -1,9 +1,9 @@
-package com.rever.ehcachedemo.cacheImpl;
+package com.rever.ehcachedemo.cache;
 
 
+import com.rever.ehcachedemo.commoncache.ICache;
+import com.rever.ehcachedemo.entity.User;
 import lombok.Getter;
-
-import java.util.Map;
 
 /**
  * @Description: 抽象缓存工厂，主要用于统一管理缓存。主要为以后更改缓存工具时提供方便。
@@ -11,22 +11,14 @@ import java.util.Map;
  * @Version: 1.0
  * @Create Date Time: 2018-08-13 16:53
  * @Update Date Time:
- *
  * @see
  */
 @Getter
 public abstract class AbstractCacheFactory {
 
-    /**
-     * 资源缓存
-     */
-   // private ICache<Attribute> attributeCache;
+    private ICache<User> userCache;
 
-
-
-    public AbstractCacheFactory(//ICache<Attribute> attributeCache,
-                               ) {
-      //  this.attributeCache = attributeCache;
-
+    public AbstractCacheFactory(ICache<User> userCache) {
+        this.userCache = userCache;
     }
 }
