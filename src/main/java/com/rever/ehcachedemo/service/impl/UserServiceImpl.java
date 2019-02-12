5803607256252377e143d23f76f69e2c9f9373dc
rever4433 <gao2524289@outlook.com>
2019-02-12 11:18:45 +0800

    新增使用缓存查询的方法

--- src/main/java/com/rever/ehcachedemo/service/impl/UserServiceImpl.java
+++ src/main/java/com/rever/ehcachedemo/service/impl/UserServiceImpl.java
@@ -4,13 +4,18 @@ package com.rever.ehcachedemo.service.impl;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.rever.ehcachedemo.cache.AbstractCacheFactory;
+import com.rever.ehcachedemo.commoncache.ICache;
 import com.rever.ehcachedemo.entity.User;
 import com.rever.ehcachedemo.mapper.UserMapper;
 import com.rever.ehcachedemo.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -21,13 +26,31 @@ import java.util.List;
  * @author gaoyakang
  * @since 2019-01-28
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    private ICache<User> userCache;
+
+    @Autowired
+    public UserServiceImpl(AbstractCacheFactory abstractCacheFactory){
+        this.userCache=abstractCacheFactory.getUserCache();
+    }
 
     @Override
-    public List<User> findUserList() {
-        Wrapper<User> wrapper=new EntityWrapper<>();
+    public List<User> findUserListInCache() {
+        Set<String> userKeys=userCache.getKeys();
+        List<User> userList=userCache.get(userKeys);
+        if(userList!=null&&userList.size()>0){
+            return userList;
+        }else {
+            Wrapper<User> wrapper = new EntityWrapper<>();
+            return this.selectList(wrapper);
+        }
+    }
+    @Override
+    public List<User> findUserList(){
+        Wrapper<User> wrapper = new EntityWrapper<>();
         return this.selectList(wrapper);
     }
 
